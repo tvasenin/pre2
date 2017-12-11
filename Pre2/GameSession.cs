1bca86f19b672cbb31888b79a4150f2eaebfdb30
Timofey Vasenin <timofey.vasenin@gmail.com>
2017-12-12 05:25:00 +0700

    Add initial GameSession

---
+++ Pre2/GameSession.cs
@@ -0,0 +1,24 @@
+﻿using Tilengine;
+
+namespace Pre2
+{
+    public class GameSession
+    {
+        private Engine engine;
+
+        private int levelIdx;
+        private byte numLife1Ups = 2;
+        private byte bonusItemsBitmap = 0;
+        private byte foodItemsMap = 0;
+        private bool isExpertMode;
+        private byte weaponStrength = 20;
+        private byte weaponType = 0;
+
+        public GameSession(Engine engine, int levelIdx, bool isExpertMode = false)
+        {
+            this.engine = engine;
+            this.levelIdx = levelIdx;
+            this.isExpertMode = isExpertMode;
+        }
+    }
+}
