48f7f6a0f52a6daa9719008e91f217b96436c2f7
Timofey Vasenin <timofey.vasenin@gmail.com>
2017-12-12 05:10:03 +0700

    Add Prng

---
+++ Pre2/Prng.cs
@@ -0,0 +1,27 @@
+﻿namespace Pre2
+{
+    public class Prng
+    {
+        private byte tmp1 = 0x5;
+        private byte tmp2 = 0x22;
+        private byte tmp3 = 0x86;
+        private ushort tmp4 = 0xE58D;
+
+        public byte GetNext()
+        {
+            tmp4 += tmp1;
+
+            tmp1 += 3;
+            tmp1 += (byte) ((tmp4 & 0xFF00) >> 8);
+
+            tmp2 += tmp3;
+            tmp2 += tmp2;
+            tmp2 += tmp1;
+
+            tmp3 ^= tmp1;
+            tmp3 ^= tmp2;
+
+            return tmp2;
+        }
+    }
+}
